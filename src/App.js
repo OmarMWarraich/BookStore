fdab06265734c0f25e24a66f48390ae4d2e9f0d4
OmarMWarraich <earthling986@gmail.com>
2022-09-28 17:45:45 +0500

    fix: .eslintrc

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
-import React from "react";
+import React from 'react';
 
-const App = () => {
-    return <div>Book Store</div>;
-    };
+const App = () => <div>Book Store</div>;
 
 export default App;
