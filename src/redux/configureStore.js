62473d6725867d4907614d8813ff6dae4ba6aee0
OmarMWarraich <earthling986@gmail.com>
2022-10-04 03:36:12 +0500

    feat: Add configureStore

---
+++ src/redux/configureStore.js
@@ -0,0 +1,16 @@
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+
+import booksReducer from "./books/books";
+
+import categoriesReducer from "./categories/categories";
+
+const rootReducer = combineReducers({
+    books: booksReducer,
+    categories: categoriesReducer,
+});
+
+const store = configureStore({
+    reducer: rootReducer,
+});
+
+export default store;
