cac8f5dcbe48ee9ef721e699f176c1e2ed7db516
OmarMWarraich <earthling986@gmail.com>
2022-10-05 04:35:20 +0500

    feat: Consume state from store w/ useSelector

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,16 +1,19 @@
-import React/* , { useState } */ from 'react';
-// import Book from './Book';
+import React from 'react';
+import { useSelector } from 'react-redux';
+import Book from './Book';
 import Form from './Form';
 
-const Books = () => (
-  <div className="books">
-    <div className="books-list">
-      {/* {booksList.map((book) => (
-          <Book key={book.id} book={book} handleBookRemove={handleBookRemove} />
-        ))} */}
+const Books = () => {
+  const BooksList = useSelector((state) => state.books);
+  return (
+    <div className="books">
+      <div className="books-list">
+        {BooksList.map((book) => (
+          <Book key={book.id} book={book} />
+        ))}
+      </div>
+      <Form />
     </div>
-    <Form />
-  </div>
-);
-
+  );
+};
 export default Books;
